3a91fa437519e3c9840750f808b03ca109eac7a6
Rosset Denis <denis.rosset@hes-so.ch>
2023-06-13 14:28:13 +0200

    pv13 agenda

--- tools/classify-sound-type/2-sec-clip.py
+++ tools/classify-sound-type/2-sec-clip.py
@@ -16,5 +16,5 @@ files = os.listdir(directory)
 
 # loop through each file in the directory
 for file in files:
-    subprocess.call(['ffmpeg', '-i', directory + '/' + file, '-c:v', 'libx264', '-crf', '22', '-map', '0', '-segment_time', time, '-reset_timestamps', '1', '-g', '30', '-sc_threshold', '0', '-force_key_frames', 'expr:gte(t,n_forced*'+str(time)+')', '-f', 'segment', directory+'new/' + file + '%05d.mp4'])
+    subprocess.call(['ffmpeg', '-i', directory + '/' + file, '-c:v', 'libx264', '-crf', '22', '-map', '0', '-segment_time', time, '-reset_timestamps', '1', '-g', '30', '-sc_threshold', '0', '-force_key_frames', 'expr:gte(t,n_forced*'+str(time)+')', '-f', 'segment', directory[:-1]+'-2sec/' + file + '%05d.mp4'])
 
